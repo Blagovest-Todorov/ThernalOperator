efb6ad2b623532c39d816a2d9eb8d25544bfbb96
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-06 09:24:45 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n))
+                .OrderByDescending(x => x)                
+                .ToArray();
+
+            int count = nums.Length >= 3 ? 3 : nums.Length; // ? thernal operator
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+        }        
+    }
+}
